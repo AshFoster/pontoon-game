6b88f8516adadaffbab50e904d566b2f96a4948a
AshFoster <ashfoster87@gmail.com>
2021-10-02 10:44:24 +0000

    Create main menu function

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -15,8 +16,49 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pontoon_data')
 
-scores = SHEET.worksheet('scores')
 
-data = scores.get_all_values()
+def main_manu():
+    """
+    The main menu of the game. Displays a list showing options for the user to
+    choose from: Play Game, See Rules, See High Scores.
+    """
+    print("Welcome! You've managed to stumble upon this terminal based")
+    print("version of the classic card game Pontoon.")
 
-print(data)
+    while True:
+        try:
+            print("\n---------")
+            print("MAIN MENU")
+            print("---------\n")
+            print("Play Pontoon:    1")
+            print("Rules:           2")
+            print("High Scores:     3\n")
+            print("Quit:            0\n")
+            menu_choice = input("Please enter your choice using the "
+                                "numbers shown: ")
+
+            if int(menu_choice) == 1:
+                # play_pontoon()
+                print("Play Pontoon")
+                break
+            elif int(menu_choice) == 2:
+                # rules()
+                print("Rules")
+                break
+            elif int(menu_choice) == 3:
+                # high_scores()
+                print("High Scores")
+                break
+            elif int(menu_choice) == 0:
+                print("You have quit the game, Goodbye.")
+                break
+            else:
+                raise ValueError()
+
+        except ValueError:
+            print(f"{menu_choice} is not valid, please try again.")
+            input("Press 'Enter' to return to the main menu.")
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+
+main_manu()
